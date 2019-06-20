b28e7003d067743353b9bec14cc8517d220feae7
L4M0S <13170563+L4M0S@users.noreply.github.com>
2019-06-20 11:52:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include<string>
 
 using namespace std;
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 typedef map<int,char> TAPE;
 typedef map<int,char>::iterator ITR;
 
@@ -28,10 +28,13 @@ char read()
 	return tape[header];
 }
 
+/////FUNCIONES BASICAS///////////////////////////////////////////////////////////////////////////////////////////////////////
 void L(int n=1)
 {
+ 
  	header=header-n;
 }
+
 void R(int n=1)
 {
 	header=header+n;
@@ -49,6 +52,7 @@ void Left2(char x)
 		L();
 	}while(read()!=x);
 }
+
 void Right2(char x)
 {
 	do
@@ -64,6 +68,7 @@ void Left2comp(char x)
 		L();
 	}while(read()==x);
 }
+
 void Right2comp(char x)
 {
 	do
@@ -72,7 +77,9 @@ void Right2comp(char x)
 	}while(read()==x);
 }
 
-void initTape()
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void initTape()	//Pone todas la casillas de la cinta en BLANCO
 {
 	for(int i= -18; i<=11; i++)
 	{
@@ -81,6 +88,8 @@ void initTape()
 	}
 	header=0;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
@@ -94,7 +103,7 @@ int main()
 	
 	L(17);
 	
-	for(int i=0;i<8;i++)
+	for(int i=0;i<8;i++)	//Inserta los simbolos de la cadena A en la cinta
 	{
 		sigma(A[i]);
 		R();
@@ -102,7 +111,7 @@ int main()
 	
 	R();
 	
-	for(int i=0;i<8;i++)
+	for(int i=0;i<8;i++)	//Inserta los simbolos de la cadena B en la cinta
 	{
 		sigma(B[i]);
 		R();
@@ -115,104 +124,95 @@ int main()
 	
 	Left2(' ');R();
 	
+	/////INICIO DE LA MAQUINA//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	print();	
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	Right2(':');//print();
-	//if(read()=='1'||read()=='0')
-	//{
-		//loop:
-		bool loop=true;
-		while(loop)
+	Right2(':');
+	
+	bool loop=true;
+	do
+	{
+		//system("pause");
+		print();
+		L();
+		print();
+		
+		if(read()==' ')
 		{
-			
-			//system("pause");
+			R();
 			print();
-			L();
-			print();
-			//print();
-			//R();L();
-			if(read()==' ')
+			loop=false;
+		}
+		else if(read()=='1')
+		{
+			R(9);
+			if(read()=='1')
 			{
-				R();
 				print();
-				loop=false;
-			}
-			else if(read()=='1')
-			{
-				//print();
-				R(9);
+				Right2('@');R();
 				if(read()=='1')
 				{
 					print();
-					Right2('@');R();
-					if(read()=='1')
-					{
-						print();
-						sigma('1');R(10);Left2(' ');sigma('1');L(20);
-					}
-					else if(read()=='0')
-					{
-						print();
-						sigma('1');R(10);Left2(' ');sigma('0');L(20);
-					}
+					sigma('1');R(10);Left2(' ');sigma('1');L(20);
 				}
 				else if(read()=='0')
 				{
 					print();
-					Right2('@');R();
-					if(read()=='1')
-					{
-						print();
-						sigma('1');R(10);Left2(' ');sigma('0');L(20);
-					}
-					else if(read()=='0')
-					{
-						print();
-						sigma('0');R(10);Left2(' ');sigma('1');L(20);
-					}
+					sigma('1');R(10);Left2(' ');sigma('0');L(20);
 				}
 			}
 			else if(read()=='0')
 			{
-				//print();
-				R(9);
+				print();
+				Right2('@');R();
 				if(read()=='1')
 				{
 					print();
-					Right2('@');R();
-					if(read()=='1')
-					{
-						print();
-						sigma('1');R(10);Left2(' ');sigma('0');L(20);
-					}
-					else if(read()=='0')
-					{
-						print();
-						sigma('0');R(10);Left2(' ');sigma('1');L(20);
-					}
+					sigma('1');R(10);Left2(' ');sigma('0');L(20);
 				}
 				else if(read()=='0')
 				{
 					print();
-					Right2('@');R();
-					if(read()=='1')
-					{
-						print();
-						sigma('0');R(10);Left2(' ');sigma('1');L(20);
-					}
-					else if(read()=='0')
-					{
-						print();
-						sigma('0');R(10);Left2(' ');sigma('0');L(20);
-					}
+					sigma('0');R(10);Left2(' ');sigma('1');L(20);
 				}
 			}
 		}
-	//}
-	//print();
-
+		else if(read()=='0')
+		{
+			R(9);
+			if(read()=='1')
+			{
+				print();
+				Right2('@');R();
+				if(read()=='1')
+				{
+					print();
+					sigma('1');R(10);Left2(' ');sigma('0');L(20);
+				}
+				else if(read()=='0')
+				{
+					print();
+					sigma('0');R(10);Left2(' ');sigma('1');L(20);
+				}
+			}
+			else if(read()=='0')
+			{
+				print();
+				Right2('@');R();
+				if(read()=='1')
+				{
+					print();
+					sigma('0');R(10);Left2(' ');sigma('1');L(20);
+				}
+				else if(read()=='0')
+				{
+					print();
+					sigma('0');R(10);Left2(' ');sigma('0');L(20);
+				}
+			}
+		}
+	}while(loop);
+	
 	cout<<endl<<endl;
 	system("pause");
 	return 0;
 }
-
